6a63562eebd09b9a900dcadb7225a90dac269600
davies368t-rgb <davies368t@gmail.com>
2025-09-03 06:51:16 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,19 @@
+bool1=True
+bool2=False
+bool3=62436>26246
+
+print(bool3)
+
+a=10
+b=63
+c=int(input("enter a number: "))
+
+if a and b and c:
+    print("True")
+else:
+    print("False")
+
+if a or c:
+    print("True")
+else:
+    print("False")
